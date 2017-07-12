6321461a30fbf907fd08f743b370aa61761749cb
Nosakhare <fredobaseki@gmail.com>
2017-07-12 02:40:55 +0100

    Displayed Name & handle

--- js/custom.js
+++ js/custom.js
@@ -11,19 +11,19 @@ function handleTweets(tweets) {
         console.log(tweets[n], n);
         html += '<div class="twitter-feeds__item">';
         html += '<!-- twitter feeds item image -->' +
-                '<div class="twitter-feeds__item__image">' +
-                '<img src="'+ el.querySelector('img').getAttribute("src") +'" alt="Twitter Avatar" class="img-circle" >' +
-                '</div>' +
-                '<!-- twitter feeds item content -->' +
-                '<div class="twitter-feeds__item__content">' +
-                    '<div class="twitter-feeds__item__content__row">' +
-                        '<span class="twitter-feeds__item__content__name">Adawale Abati</span>' +
-                        '<span class="twitter-feeds__item__content__handle">@Ace_KYD</span>' +
-                    '</div>' +
-                    '<div class="twitter-feeds__item__content__row">' +
-                        '<p class="twitter-feeds__item__content__text">'+ el.querySelector('p.tweet').innerHTML +'</p>'+
-                    '</div>';
-                '</div>';
+            '<div class="twitter-feeds__item__image">' +
+            '<img src="'+ el.querySelector('img').getAttribute("src") +'" alt="Twitter Avatar" class="img-circle" >' +
+            '</div>' +
+            '<!-- twitter feeds item content -->' +
+            '<div class="twitter-feeds__item__content">' +
+            '<div class="twitter-feeds__item__content__row">' +
+            '<span class="twitter-feeds__item__content__name">' + el.querySelector('[data-scribe="element:name"]').innerHTML + '</span>' +
+            '<span class="twitter-feeds__item__content__handle">'+ el.querySelector('[data-scribe="element:screen_name"]').innerHTML + '</span>' +
+            '</div>' +
+            '<div class="twitter-feeds__item__content__row">' +
+            '<p class="twitter-feeds__item__content__text">'+ el.querySelector('p.tweet').innerHTML +'</p>'+
+            '</div>';
+        '</div>';
         html += '</div></div>';
         n++;
     }
